f781a21d99ef8940e5c5ee05b315c6d4da08ed07
Dilla <ummuadilla1@gmail.com>
2026-01-07 20:50:18 +0700

    Update Payment.kt

--- app/src/main/java/com/example/jelajapediapp/Payment.kt
+++ app/src/main/java/com/example/jelajapediapp/Payment.kt
@@ -1,1 +1,9 @@
-
+data class Payment(
+    val id: String,
+    val bookingId: String,
+    val amount: Double,
+    val paymentMethod: String, // "CREDIT_CARD", "DEBIT_CARD", "E_WALLET", "BANK_TRANSFER"
+    val status: String, // "PENDING", "SUCCESS", "FAILED"
+    val transactionDate: String,
+    val orderNumber: String
+)
