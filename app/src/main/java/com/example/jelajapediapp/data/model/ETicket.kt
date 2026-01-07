98260026f2e67b3b299757e51fb6295d87087c15
Dilla <ummuadilla1@gmail.com>
2026-01-07 21:13:07 +0700

    JelajahPedia

---
+++ app/src/main/java/com/example/jelajapediapp/data/model/ETicket.kt
@@ -0,0 +1,15 @@
+package com.example.jelajapedi.data.model
+
+data class ETicket(
+    val ticketId: String,
+    val bookingId: String,
+    val passengerName: String,
+    val destinationName: String,
+    val departureDate: String,
+    val departureTime: String,
+    val qrCode: String, // URL atau encoded data QR Code
+    val ticketStatus: String, // "VALID", "USED", "EXPIRED"
+    val ticketNumber: String,
+    val seatNumber: String?,
+    val generatedDate: String
+)
